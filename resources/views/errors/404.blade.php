aa8496e4961beaada9a6e01630ed3733d0282a44
Victorien Plancke <vplancke@norauto.com>
2017-02-01 13:59:08 +0100

    Error message edit

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row">
     <div class="col-md-12"> 
-        <h2> 404 Page non trouvée. </h2>
+        <h2> 404 - Page non trouvée. </h2>
 
         <p><img src="http://image.noelshack.com/fichiers/2016/24/1466366648-risitas115.png" height="400px"/></p>
     </div>
