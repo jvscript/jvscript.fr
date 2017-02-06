71a8031171d38d4f7ff5895d97076e8f87eed093
Vito <fb@vito.fr.nf>
2017-02-06 22:09:46 +0100

    [view] mettre des liens sur les thumb et pas des onclick

--- resources/views/home/script.blade.php
+++ resources/views/home/script.blade.php
@@ -1,70 +1,73 @@
 <div class="col-xs-6 col-sm-3 col-md-3">
-    <div class="thumbnail" onclick="window.location ='{{route('script.show',['slug' => $script->slug ])}}';" style="cursor: pointer;">
-        <div class="hover-caption">
-          <p class="pull-left">
-            <h4>{{$script->name}}</h4>
-          <p class="pull-left">
-              <?php $note = round($script->note * 2) / 2; ?>
-              @for ($i = 1; $i <= $note ; $i++)
-              <i class="fa fa-star" aria-hidden="true"></i>
-              @endfor
+    <div class="thumbnail">
+        <a href="{{route('script.show',['slug' => $script->slug ])}}" class="">
 
-              <?php $stop = $i; ?>
+            <div class="hover-caption">
+                <p class="pull-left">
+                <h4>{{$script->name}}</h4>
+                <p class="pull-left">
+                    <?php $note = round($script->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    @endfor
 
-              @for ($i ; $i <= 5 ; $i++)
-              @if($i == $stop && $note > ( $i -1 ) )
-              <i class="fa fa-star-half-o" aria-hidden="true"></i>
-              @else
-              <i class="fa fa-star-o" aria-hidden="true"></i>
-              @endif
+                    <?php $stop = $i; ?>
 
-              @endfor
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    @else
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    @endif
 
-          </p>
-          <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
-          <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
-          <p class="text-right">
-              <span class=" label label-script">Script</span>
-          </p>
-        @if($script->description != null)
-        <p class="desc">{{str_limit($script->description,350)}}</p>
-        @else
-         @if($script->autor != null)
-        <p>Proposé par {{$script->autor}}</p>
-         @endif
-        <p>
-             Ajouté le :   {{$script->created_at->format('d/m/Y')}}
-        </p>
-        @endif
-        </div>
-        <a href="{{route('script.show',['slug' => $script->slug ])}}">
+                    @endfor
+
+                </p>
+                <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
+                <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
+                <p class="text-right">
+                    <span class=" label label-script">Script</span>
+                </p>
+                @if($script->description != null)
+                <p class="desc">{{str_limit($script->description,350)}}</p>
+                @else
+                @if($script->autor != null)
+                <p>Proposé par {{$script->autor}}</p>
+                @endif
+                <p>
+                    Ajouté le :   {{$script->created_at->format('d/m/Y')}}
+                </p>
+                @endif
+            </div>
+
             <?php $src = $script->photo_url == null ? "/assets/images/script.jpg" : $script->photo_url ?>
-            <div class="image "> <img src="{{$src}}" class="  " alt="{{$script->name}} logo" /></a> </div>
-    <div class="caption">
-        <h4>{{$script->name}}</h4>
-        <p class="pull-left">
-            <?php $note = round($script->note * 2) / 2; ?>
-            @for ($i = 1; $i <= $note ; $i++)
-            <i class="fa fa-star" aria-hidden="true"></i>
-            @endfor
+            <div class="image "> <img src="{{$src}}" class="  " alt="{{$script->name}} logo" /> </div>
+            <div class="caption">
+                <h4>{{$script->name}}</h4>
+                <p class="pull-left">
+                    <?php $note = round($script->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    @endfor
 
-            <?php $stop = $i; ?>
+                    <?php $stop = $i; ?>
 
-            @for ($i ; $i <= 5 ; $i++)
-            @if($i == $stop && $note > ( $i -1 ) )
-            <i class="fa fa-star-half-o" aria-hidden="true"></i>
-            @else
-            <i class="fa fa-star-o" aria-hidden="true"></i>
-            @endif
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    @else
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    @endif
 
-            @endfor
+                    @endfor
 
-        </p>
-        <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
-        <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
-        <p class="text-right">
-            <span class=" label label-script">Script</span>
-        </p>
+                </p>
+                <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
+                <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
+                <p class="text-right">
+                    <span class=" label label-script">Script</span>
+                </p>
+            </div>
+        </a>
     </div>
-</div>
 </div>
--- resources/views/home/skin.blade.php
+++ resources/views/home/skin.blade.php
@@ -1,71 +1,73 @@
-<div class="col-xs-6 col-sm-3 col-md-3"  onclick="window.location ='{{route('skin.show',['slug' => $skin->slug ])}}';" style="cursor: pointer;">
+<div class="col-xs-6 col-sm-3 col-md-3">
     <div class="thumbnail">
-      <div class="hover-caption">
-        <p class="pull-left">
-          <h4>{{$skin->name}}</h4>
-        <p class="pull-left">
-            <?php $note = round($script->note * 2) / 2; ?>
-            @for ($i = 1; $i <= $note ; $i++)
-            <i class="fa fa-star" aria-hidden="true"></i>
-            @endfor
+        <a href="{{route('skin.show',['slug' => $skin->slug ])}}" class="">
+            <div class="hover-caption">
+                <p class="pull-left">
+                <h4>{{$skin->name}}</h4>
+                <p class="pull-left">
+                    <?php $note = round($skin->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    @endfor
 
-            <?php $stop = $i; ?>
+                    <?php $stop = $i; ?>
 
-            @for ($i ; $i <= 5 ; $i++)
-            @if($i == $stop && $note > ( $i -1 ) )
-            <i class="fa fa-star-half-o" aria-hidden="true"></i>
-            @else
-            <i class="fa fa-star-o" aria-hidden="true"></i>
-            @endif
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    @else
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    @endif
 
-            @endfor
+                    @endfor
 
-        </p>
-        <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
-        <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
-        <p class="text-right">
-            <span class=" label label-skin">Script</span>
-        </p>
-      @if($script->description != null)
-      <p class="desc">{{str_limit($script->description,350)}}</p>
-      @else
-       @if($script->autor != null)
-      <p>Proposé par {{$script->autor}}</p>
-       @endif
-      <p>
-           Ajouté le :   {{$script->created_at->format('d/m/Y')}}
-      </p>
-      @endif
-      </div>
-        <a href="{{route('skin.show',['slug' => $skin->slug ])}}">
+                </p>
+                <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$skin->install_count}} </p>
+                <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$skin->autor}} </p>
+                <p class="text-right">
+                    <span class=" label label-skin">Skin</span>
+                </p>
+                @if($skin->description != null)
+                <p class="desc">{{str_limit($skin->description,350)}}</p>
+                @else
+                @if($skin->autor != null)
+                <p>Proposé par {{$skin->autor}}</p>
+                @endif
+                <p>
+                    Ajouté le :   {{$skin->created_at->format('d/m/Y')}}
+                </p>
+                @endif
+            </div>
+
             <?php $src = $skin->photo_url == null ? "/assets/images/skin.jpg" : $skin->photo_url ?>
             <div class="image">
-            <img src="{{$src}}" class=" " alt="{{$skin->name}} logo" /></a>
+                <img src="{{$src}}" class=" " alt="{{$skin->name}} logo" />
+            </div>
+            <div class="caption">
+                <h4>{{$skin->name}}</h4>
+                <p class="pull-left">
+                    <?php $note = round($skin->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    @endfor
+
+                    <?php $stop = $i; ?>
+
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                    @else
+                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                    @endif
+
+                    @endfor
+                </p>
+                <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$skin->install_count}} </p>
+                <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$skin->autor}} </p>
+                <p class="text-right">
+                    <span class=" label label-skin">Skin</span>
+                </p>
+            </div>
+        </a>
     </div>
-    <div class="caption">
-        <h4>{{$skin->name}}</h4>
-        <p class="pull-left">
-            <?php $note = round($skin->note * 2) / 2; ?>
-            @for ($i = 1; $i <= $note ; $i++)
-            <i class="fa fa-star" aria-hidden="true"></i>
-            @endfor
-
-            <?php $stop = $i; ?>
-
-            @for ($i ; $i <= 5 ; $i++)
-            @if($i == $stop && $note > ( $i -1 ) )
-            <i class="fa fa-star-half-o" aria-hidden="true"></i>
-            @else
-            <i class="fa fa-star-o" aria-hidden="true"></i>
-            @endif
-
-            @endfor
-        </p>
-        <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$skin->install_count}} </p>
-        <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$skin->autor}} </p>
-        <p class="text-right">
-            <span class=" label label-skin">Skin</span>
-        </p>
-    </div>
-</div>
 </div>
