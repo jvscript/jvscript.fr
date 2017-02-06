2a1df117aea3f6cee4c46f9d31c054f4a1e22555
Nass O <nowzy1@gmail.com>
2017-02-06 16:56:18 +0100

    thumbnail

--- resources/views/home/script.blade.php
+++ resources/views/home/script.blade.php
@@ -1,9 +1,33 @@
 <div class="col-xs-6 col-sm-3 col-md-3">
     <div class="thumbnail" onclick="window.location ='{{route('script.show',['slug' => $script->slug ])}}';" style="cursor: pointer;">
         <div class="hover-caption">
-        <h4>{{$script->name}}  </h4>
+          <p class="pull-left">
+            <h4>{{$script->name}}</h4>
+          <p class="pull-left">
+              <?php $note = round($script->note * 2) / 2; ?>
+              @for ($i = 1; $i <= $note ; $i++)
+              <i class="fa fa-star" aria-hidden="true"></i>
+              @endfor
+
+              <?php $stop = $i; ?>
+
+              @for ($i ; $i <= 5 ; $i++)
+              @if($i == $stop && $note > ( $i -1 ) )
+              <i class="fa fa-star-half-o" aria-hidden="true"></i>
+              @else
+              <i class="fa fa-star-o" aria-hidden="true"></i>
+              @endif
+
+              @endfor
+
+          </p>
+          <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
+          <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
+          <p class="text-right">
+              <span class=" label label-script">Script</span>
+          </p>
         @if($script->description != null)
-        <p>{{str_limit($script->description,350)}}</p>
+        <p class="desc">{{str_limit($script->description,350)}}</p>
         @else
          @if($script->autor != null)
         <p>Proposé par {{$script->autor}}</p>
--- resources/views/home/skin.blade.php
+++ resources/views/home/skin.blade.php
@@ -1,19 +1,42 @@
 <div class="col-xs-6 col-sm-3 col-md-3"  onclick="window.location ='{{route('skin.show',['slug' => $skin->slug ])}}';" style="cursor: pointer;">
     <div class="thumbnail">
-        <div class="hover-caption">
-            <h4>{{$skin->name}}  </h4>
-            @if($skin->description != null)
-            <p>{{str_limit($skin->description,350)}}</p>
-            @else
-            @if($skin->autor != null)
-            <p>Proposé par {{$skin->autor}}</p>
-            @endif
-            <p>
-                Ajouté le :   {{$skin->created_at->format('d/m/Y')}}
-            </p>
-            @endif
-        </div>
+      <div class="hover-caption">
+        <p class="pull-left">
+          <h4>{{$skin->name}}</h4>
+        <p class="pull-left">
+            <?php $note = round($script->note * 2) / 2; ?>
+            @for ($i = 1; $i <= $note ; $i++)
+            <i class="fa fa-star" aria-hidden="true"></i>
+            @endfor
 
+            <?php $stop = $i; ?>
+
+            @for ($i ; $i <= 5 ; $i++)
+            @if($i == $stop && $note > ( $i -1 ) )
+            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+            @else
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+            @endif
+
+            @endfor
+
+        </p>
+        <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
+        <p class="pull-left"><i class="fa fa-user" aria-hidden="true"></i> {{$script->autor}} </p>
+        <p class="text-right">
+            <span class=" label label-script">Script</span>
+        </p>
+      @if($script->description != null)
+      <p class="desc">{{str_limit($script->description,350)}}</p>
+      @else
+       @if($script->autor != null)
+      <p>Proposé par {{$script->autor}}</p>
+       @endif
+      <p>
+           Ajouté le :   {{$script->created_at->format('d/m/Y')}}
+      </p>
+      @endif
+      </div>
         <a href="{{route('skin.show',['slug' => $skin->slug ])}}">
             <?php $src = $skin->photo_url == null ? "/assets/images/skin.jpg" : $skin->photo_url ?>
             <div class="image">
