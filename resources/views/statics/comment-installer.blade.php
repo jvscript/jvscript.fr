69627f8b5cdc8ebd1b4bdc93592ee72f1f911d2f
Nass O <nowzy1@gmail.com>
2017-02-13 17:35:01 +0100

    syntax orthographe

--- resources/views/statics/comment-installer.blade.php
+++ resources/views/statics/comment-installer.blade.php
@@ -6,25 +6,20 @@
 <div class="row">
     <div class="col-md-12">
 
-     <h1 id="aide">Aide</h1>
-
-
-
 <h2 id="comment-installer-un-script">Comment installer un script ?</h2>
 
 <p><strong>Prérequis :</strong></p>
 
 <ul>
-<li>Pour Chrome: Installer <a href="https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=fr">TamperMonkey</a></li>
-<li>Pour Firefox: Installer <a href="https://addons.mozilla.org/fr/firefox/addon/greasemonkey/">GreaseMonkey</a></li>
+<li><p>Pour Chrome: Installer <a href="https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=fr">TamperMonkey</a></p></li>
+<li><p>Pour Firefox: Installer <a href="https://addons.mozilla.org/fr/firefox/addon/greasemonkey/">GreaseMonkey</a></p></li>
 </ul>
 
+
 <p><strong>Installation :</strong> <br>
-Ensuite vous pouvez choisir un script dans la liste présente sur la page d’accueil, aller sur sa page, et cliquer sur le bouton <em>Installer</em> au-dessus de sa description sur sa page. Vous serez redirigé sur une page ou il ne restera qu’à cliquer sur <em>Installer</em>. </p>
+Il vous suffit de cliquer sur le script de votre choix dans la page d'accueil du site, puis de cliquer sur <em>Installer</em>. Vous serez redirigé sur une page où il ne vous restera qu’à cliquer sur <em>Installer</em>. </p>
 
 <p><img src="http://image.noelshack.com/fichiers/2017/07/1486990384-screenshot-1.png" alt="Installer un Script sur TamperMonkey" title=""></p>
-
-<p><strong>Si la page ne s’ouvre pas :</strong></p>
 
 <h2 id="comment-installer-un-skin">Comment installer un skin ?</h2>
 
@@ -36,31 +31,31 @@ Ensuite vous pouvez choisir un script dans la liste présente sur la page d’ac
 </ul>
 
 <p><strong>Installation :</strong> <br>
-Ensuite vous pouvez aller choisir un skin sur la page d’accueil du site, cliquer sur celui choisis et cliquer sur le bouton <em>Installer</em>. Vous serez redirigé sur la page Stylish ou il y aura un bouton pour installer un script: Cliquer dessus pour terminer l’opération. <br>
+Il vous suffit de cliquer sur le script de votre choix dans la page d'accueil du site, puis de cliquer sur <em>Installer</em>. Vous serez redirigé sur la page Stylish du skin en question. Il ne vous restera plus qu'à cliquer sur le bouton <em>Install with Stylish</em>.<br>
 <img src="http://image.noelshack.com/fichiers/2017/07/1486991277-screenshot-2.png" alt="Installer un skin avec Stylish" title=""></p>
 
 
 
 <h2 id="aidez-moi-mon-script-plante">Aidez-moi ! Mon script plante !</h2>
 
-<p>Des extensions, particuliérement les bloqueurs de pubs (Ad-Block, uBlock…) peuvent bloquer les scripts: Dans ce cas nous vous conseillons de vérifier cela en enteignant votre bloqueur de pubs. <br>
-Toutefois si vous avez plusieurs scripts ils peuvent interférer et provoquer des problèmes, vous pouvez vérifier cela en vérifiant la console du navigateur (Ctrl+Maj+J).</p>
+<p>Des extensions, particulièrement les bloqueurs de pubs (Ad-Block, uBlock…) peuvent bloquer les scripts. Dans ce cas nous vous conseillons de vérifier cela en éteignant votre bloqueur de pubs. <br>
+Aussi, des scripts combinés ensemble peuvent provoquer des erreurs, vous pouvez vérifier cela en vérifiant la console du navigateur (Ctrl+Maj+J).</p>
 
 <h2 id="comment-mettre-à-jour-les-scripts-et-skins">Comment mettre à jour les scripts et skins ?</h2>
 
-<p>Sur certains scripts vous serez prévenus des mises à jours et dans la plupart des cas elles seront automatiques (Une fois par jour).  <br>
+<p>Sur certains scripts vous serez prévenus des mises à jour et dans la plupart des cas elles seront automatiques (une fois par jour).  <br>
 Si vous voulez forcer la mise à jour il suffit d’aller dans les options de TamperMonkey (Ou GreaseMonkey).</p>
 
 <h2 id="quels-sont-les-risques-à-installer-un-script">Quels sont les risques à installer un Script ?</h2>
 
-<p><strong>Modérations :</strong> <br>
+<p><strong>Modération :</strong> <br>
 Sur chaque script est indiqué à côté du bouton <em>Installer</em> une icone indiquant si le script est interdit par la modération ou pas.  <br>
-<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-3.png" alt="safe" title=""> Le script est sécurisé, il y a aucun problème à l’utiliser ! <br>
-<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-5.png" alt="always" title=""> Nous ne sommes pas sur que ce script peut convenir avec la modération. <br>
-<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-4.png" alt="danger" title=""> L’utilisation de ce script peut mener à des sanctions !</p>
+<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-3.png" alt="safe" title=""> Le script est autorisé par la modération, il y a aucun problème à l’utiliser ! <br>
+<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-5.png" alt="always" title=""> Nous ne sommes pas sûrs que ce script soit sans danger. Nous vous conseillons de contacter la modération de jeuxvideo.com .<br>
+<img src="http://image.noelshack.com/fichiers/2017/07/1486991578-screenshot-4.png" alt="danger" title=""> L’utilisation de ce script est dangereux. La modération ne tolère pas ce genre de script, son utilisation peut mener à des sanctions.</p>
 
 <p><strong>Piratage:</strong>  <br>
-Les scripts sont tésté avant la publication, néanmoins on est pas responsable si le script est mis à jour avec un nouveau code malicieux.</p>
+Les scripts sont testés avant la publication, néanmoins nous ne sommes pas responsables si le script est mis à jour avec un nouveau code malicieux.</p>
     </div>
 </div>
 
