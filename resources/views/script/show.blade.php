bf33c06136787383a54e4d2645c8b9b9d3883846
Vito <fb@vito.fr.nf>
2017-02-19 17:51:23 +0100

    [fix] html issue show script/skin

--- resources/views/script/show.blade.php
+++ resources/views/script/show.blade.php
@@ -54,174 +54,181 @@
 <div class="row">
 
     <div class="col-md-6">
-      <div class="panel-body">
-        <div class="desc-img">
-        <p>
-            @if ( $script->photo_url != null )
-            <a href="{{$script->photo_url}}"  data-toggle="modal" data-target="#myModal">
-                <img class="img-thumbnail img-responsive" src="{{$script->photo_url}}" style="max-height: 300px;max-width: 400px;" alt="{{$script->name}} logo" />
-            </a>
-</div>
+        <div class="panel-body">
+            <div class="desc-img">
+                <p>
+                    @if ( $script->photo_url != null )
+                    <a href="{{$script->photo_url}}"  data-toggle="modal" data-target="#myModal">
+                        <img class="img-thumbnail img-responsive" src="{{$script->photo_url}}" style="max-height: 300px;max-width: 400px;" alt="{{$script->name}} logo" />
+                    </a>
+                    @else
+                    <img class="img-thumbnail img-responsive" src="/assets/images/script.jpg" style="max-height: 300px;" />
+                    @endif
+                </p>
+            </div>
             <!-- Modal -->
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
 
-                    <div class="modal-body text-center">
-                        <img class="img-thumbnail img-responsive" src="{{$script->photo_url}}" style="" alt="{{$script->name}} logo" />
+                        <div class="modal-body text-center">
+                            <img class="img-thumbnail img-responsive" src="{{$script->photo_url}}" style="" alt="{{$script->name}} logo" />
+                        </div>
+
                     </div>
-
                 </div>
             </div>
+
+
+
+
+            <div class="stats">
+                <p>
+                    <b> Ajouté le : </b>  {{$script->created_at->format('d/m/Y')}}
+                </p>
+
+                @if(null != $script->version)
+                <p>
+                    <b> Version : </b>  {{$script->version}}
+                </p>
+                @endif
+
+                @if(null != $script->last_update)
+                <p>
+                    <b> Mise à jour le : </b>  {{$script->last_update->format('d/m/Y')}}
+                </p>
+                @endif
+                @if(null != $script->user_id)
+                <p>
+                    <b> Auteur : </b> {{$script->user()->first()->name}}
+                </p>
+                @elseif($script->autor != null)
+                <p>
+                    <b> Auteur : </b> {{$script->autor}}
+                </p>
+                @endif
+
+                <p>
+                    <b> Note : </b>
+                    <?php $note = round($script->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i  ])}}"><i class="fa fa-star" aria-hidden="true"></i></a>
+                    @endfor
+                    <?php $stop = $i; ?>
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i  ])}}"><i class="fa fa-star-half-o" aria-hidden="true"></i></a>
+                    @else
+                    <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i ])}}"><i class="fa fa-star-o" aria-hidden="true"></i></a>
+                    @endif
+
+                    @endfor
+
+                    ({{$script->note_count}} votes)
+                </p>
+
+                <p>
+                    <b>  Install : </b>   {{$script->install_count}} fois
+                </p>
+
+                @if ( $script->repo_url != null )
+                <p>
+                    <b>  Contribuer : <a target="_blank" href="{{$script->repo_url}}">{{str_limit($script->repo_url,40)}}</a>  </b>
+                </p>
+                @endif
+
+                @if ( $script->topic_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$script->topic_url}}">Voir le topic jvc  <i class="fa fa-gamepad"></i></a>  </b>
+                </p>
+                @endif
+
+                @if ( $script->website_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$script->website_url}}">Voir le site web  <i class="fa fa-globe"></i></a>  </b>
+                </p>
+                @endif
+
+                @if ( $script->don_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$script->don_url}}">Faire un don au développeur <i class="fa fa-heart"></i></a>  </b>
+                </p>
+                @endif
+            </div>
         </div>
-
-        @else
-        <img class="img-thumbnail img-responsive" src="/assets/images/script.jpg" style="max-height: 300px;" />
-        @endif
-        </p>
- 
-              <div class="stats">
-        <p>
-            <b> Ajouté le : </b>  {{$script->created_at->format('d/m/Y')}}
-        </p>
-
-        @if(null != $script->version)
-        <p>
-            <b> Version : </b>  {{$script->version}}
-        </p>
-        @endif
-
-        @if(null != $script->last_update)
-        <p>
-            <b> Mise à jour le : </b>  {{$script->last_update->format('d/m/Y')}}
-        </p>
-        @endif
-        @if(null != $script->user_id)
-        <p>
-            <b> Auteur : </b> {{$script->user()->first()->name}}
-        </p>
-        @elseif($script->autor != null)
-        <p>
-            <b> Auteur : </b> {{$script->autor}}
-        </p>
-        @endif
-
-        <p>
-            <b> Note : </b>
-            <?php $note = round($script->note * 2) / 2; ?>
-            @for ($i = 1; $i <= $note ; $i++)
-            <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i  ])}}"><i class="fa fa-star" aria-hidden="true"></i></a>
-            @endfor
-            <?php $stop = $i; ?>
-            @for ($i ; $i <= 5 ; $i++)
-            @if($i == $stop && $note > ( $i -1 ) )
-            <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i  ])}}"><i class="fa fa-star-half-o" aria-hidden="true"></i></a>
-            @else
-            <a href="{{route('script.note',['slug' => $script->slug , 'note' => $i ])}}"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-            @endif
-
-            @endfor
-
-            ({{$script->note_count}} votes)
-        </p>
-
-        <p>
-            <b>  Install : </b>   {{$script->install_count}} fois
-        </p>
-
-        @if ( $script->repo_url != null )
-        <p>
-            <b>  Contribuer : <a target="_blank" href="{{$script->repo_url}}">{{str_limit($script->repo_url,40)}}</a>  </b>
-        </p>
-        @endif
-
-        @if ( $script->topic_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$script->topic_url}}">Voir le topic jvc  <i class="fa fa-gamepad"></i></a>  </b>
-        </p>
-        @endif
-
-        @if ( $script->website_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$script->website_url}}">Voir le site web  <i class="fa fa-globe"></i></a>  </b>
-        </p>
-        @endif
-
-        @if ( $script->don_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$script->don_url}}">Faire un don au développeur <i class="fa fa-heart"></i></a>  </b>
-        </p>
-        @endif
-      </div>
-</div>
     </div>
 
     <div class="col-md-6">
-      <div class="panel-body desc">
-        @if( $script->description != '' )
-       {!! (($script->description )) !!}
-        @endif
-      </div>
+        <div class="panel-body desc">
+            @if( $script->description != '' )
+            {!! (($script->description )) !!}
+            @endif
+        </div>
     </div>
-
 </div>
 
 
 @if ((Auth::check() && Auth::user()->isAdmin()))
 <div class="row">
     <div class="col-md-6">
-      <div class="panel-body">
-          <div class="admin">
-        @if($script->status == 0)
-        <div class="alert alert-warning" role="alert">
-            Ce script est en attente de validation
-        </div>
-        @elseif($script->status == 1)
-        <div class="alert alert-success" role="alert">
-            Ce script a été validé.
-        </div>
-        @elseif($script->status == 2)
-        <div class="alert alert-danger" role="alert">
-            Ce script a été refusé.
-        </div>
-        @endif
-        <p>
-            Edition :
-            <a href="{{route('script.edit',$script->slug)}}" class="btn btn-primary">Editer</a>
-            <a href="{{route('script.delete',$script->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
+        <div class="panel-body">
+            <div class="admin">
+                @if($script->status == 0)
+                <div class="alert alert-warning" role="alert">
+                    Ce script est en attente de validation
+                </div>
+                @elseif($script->status == 1)
+                <div class="alert alert-success" role="alert">
+                    Ce script a été validé.
+                </div>
+                @elseif($script->status == 2)
+                <div class="alert alert-danger" role="alert">
+                    Ce script a été refusé.
+                </div>
+                @endif
+                <p>
+                    Edition :
+                    <a href="{{route('script.edit',$script->slug)}}" class="btn btn-primary">Editer</a>
+                    <a href="{{route('script.delete',$script->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
 
-            Validation :
-            <!--_TODO : confirm dialog-->
-            <a href="{{route('script.validate',$script->slug)}}" class="btn btn-success" data-toggle="confirmation" >Valider</a>
-            <a href="{{route('script.refuse',$script->slug)}}" class="btn btn-warning" data-toggle="confirmation" >Refuser</a>
-        </p>
+                    Validation :
+                    <!--_TODO : confirm dialog-->
+                    <a href="{{route('script.validate',$script->slug)}}" class="btn btn-success" data-toggle="confirmation" >Valider</a>
+                    <a href="{{route('script.refuse',$script->slug)}}" class="btn btn-warning" data-toggle="confirmation" >Refuser</a>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 @elseif ((Auth::check() && Auth::user()->id == $script->user_id))
 <div class="row">
     <div class="col-md-6">
-        @if($script->status == 0)
-        <div class="alert alert-warning" role="alert">
-            Ce script est en attente de validation
+        <div class="panel-body">
+            <div class="admin">
+                @if($script->status == 0)
+                <div class="alert alert-warning" role="alert">
+                    Votre script est en attente de validation
+                </div>
+                @elseif($script->status == 1)
+                <div class="alert alert-success" role="alert">
+                    Votre script a été validé.
+                </div>
+                @elseif($script->status == 2)
+                <div class="alert alert-danger" role="alert">
+                    Votre script a été refusé.
+                </div>
+                @endif
+                <p>
+                    Action :
+                    <a href="{{route('script.edit',$script->slug)}}" class="btn btn-primary">Editer</a>
+                    <a href="{{route('script.delete',$script->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
+                </p>
+
+            </div>
         </div>
-        @elseif($script->status == 1)
-        <div class="alert alert-success" role="alert">
-            Ce script a été validé.
-        </div>
-        @elseif($script->status == 2)
-        <div class="alert alert-danger" role="alert">
-            Ce script a été refusé.
-        </div>
-        @endif
-        <p>
-            Edition :
-            <a href="{{route('script.edit',$script->slug)}}" class="btn btn-primary">Editer</a>
-            <a href="{{route('script.delete',$script->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
-        </p>
-    </div>
-    </div>
     </div>
 </div>
+
 @endif
 
 
--- resources/views/skin/show.blade.php
+++ resources/views/skin/show.blade.php
@@ -32,107 +32,108 @@
 
     <div class="col-md-6">
         <div class="panel-body">
-          <div class="desc-img">
+            <div class="desc-img">
 
-        <p>
-            @if ( $skin->photo_url != null )
-            <a href="{{$skin->photo_url}}" target="_blank" data-toggle="modal" data-target="#myModal">
-                <img class="img-thumbnail img-responsive" src="{{$skin->photo_url}}" style="max-height: 300px;max-width: 400px;" alt="{{$skin->name}} logo" />
-            </a>
-</div>
+                <p>
+                    @if ( $skin->photo_url != null )
+                    <a href="{{$skin->photo_url}}" target="_blank" data-toggle="modal" data-target="#myModal">
+                        <img class="img-thumbnail img-responsive" src="{{$skin->photo_url}}" style="max-height: 300px;max-width: 400px;" alt="{{$skin->name}} logo" />
+                    </a>
+                    @else
+                    <img class="img-thumbnail img-responsive" src="/assets/images/skin.jpg" style="max-height: 300px;" />
+                    @endif
+                </p>
+            </div>
             <!-- Modal -->
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
 
-                    <div class="modal-body text-center">
-                        <img class="img-thumbnail img-responsive" src="{{$skin->photo_url}}" style="" alt="{{$skin->name}} logo" />
+                        <div class="modal-body text-center">
+                            <img class="img-thumbnail img-responsive" src="{{$skin->photo_url}}" style="" alt="{{$skin->name}} logo" />
+                        </div>
+
                     </div>
-
                 </div>
             </div>
+
+
+
+            <div class="stats">
+                <p>
+                    <b> Ajouté le : </b>  {{$skin->created_at->format('d/m/Y')}}
+                </p>
+                @if(null != $skin->last_update)
+                <p>
+                    <b> Mise à jour le : </b>  {{$skin->last_update->format('d/m/Y')}}
+                </p>
+                @endif
+                @if(null != $skin->user_id)
+                <p>
+                    <b> Auteur : </b> {{$skin->user()->first()->name}}
+                </p>
+                @elseif($skin->autor != null)
+                <p>
+                    <b> Auteur : </b> {{$skin->autor}}
+                </p>
+                @endif
+
+                <p>
+                    <b> Note : </b>
+                    <?php $note = round($skin->note * 2) / 2; ?>
+                    @for ($i = 1; $i <= $note ; $i++)
+                    <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i  ])}}"><i class="fa fa-star" aria-hidden="true"></i></a>
+                    @endfor
+                    <?php $stop = $i; ?>
+                    @for ($i ; $i <= 5 ; $i++)
+                    @if($i == $stop && $note > ( $i -1 ) )
+                    <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i  ])}}"><i class="fa fa-star-half-o" aria-hidden="true"></i></a>
+                    @else
+                    <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i ])}}"><i class="fa fa-star-o" aria-hidden="true"></i></a>
+                    @endif
+
+                    @endfor
+
+                    ({{$skin->note_count}} votes)
+                </p>
+
+                <p>
+                    <b>  Install : </b>   {{$skin->install_count}} fois
+                </p>
+
+                @if ( $skin->repo_url != null )
+                <p>
+                    <b>  Contribuer : <a target="_blank" href="{{$skin->repo_url}}">{{str_limit($skin->repo_url,40)}}</a>  </b>
+                </p>
+                @endif
+
+                @if ( $skin->topic_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$skin->topic_url}}">Voir le topic jvc  <i class="fa fa-gamepad"></i></a>  </b>
+                </p>
+                @endif
+
+                @if ( $skin->website_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$skin->website_url}}">Voir le site web  <i class="fa fa-globe"></i></a>  </b>
+                </p>
+                @endif
+
+                @if ( $skin->don_url != null )
+                <p>
+                    <b>   <a target="_blank" class="btn btn-default" href="{{$skin->don_url}}">Faire un don au développeur  <i class="fa fa-heart"></i></a>  </b>
+                </p>
+                @endif
+            </div>
         </div>
-
-        @else
-        <img class="img-thumbnail img-responsive" src="/assets/images/skin.jpg" style="max-height: 300px;" />
-        @endif
-        </p>
-
-                <div class="stats">
-        <p>
-            <b> Ajouté le : </b>  {{$skin->created_at->format('d/m/Y')}}
-        </p>
-        @if(null != $skin->last_update)
-        <p>
-            <b> Mise à jour le : </b>  {{$skin->last_update->format('d/m/Y')}}
-        </p>
-        @endif
-        @if(null != $skin->user_id)
-        <p>
-            <b> Auteur : </b> {{$skin->user()->first()->name}}
-        </p>
-        @elseif($skin->autor != null)
-        <p>
-            <b> Auteur : </b> {{$skin->autor}}
-        </p>
-        @endif
-
-        <p>
-            <b> Note : </b>
-            <?php $note = round($skin->note * 2) / 2; ?>
-            @for ($i = 1; $i <= $note ; $i++)
-            <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i  ])}}"><i class="fa fa-star" aria-hidden="true"></i></a>
-            @endfor
-            <?php $stop = $i; ?>
-            @for ($i ; $i <= 5 ; $i++)
-            @if($i == $stop && $note > ( $i -1 ) )
-            <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i  ])}}"><i class="fa fa-star-half-o" aria-hidden="true"></i></a>
-            @else
-            <a href="{{route('skin.note',['slug' => $skin->slug , 'note' => $i ])}}"><i class="fa fa-star-o" aria-hidden="true"></i></a>
-            @endif
-
-            @endfor
-
-            ({{$skin->note_count}} votes)
-        </p>
-
-        <p>
-            <b>  Install : </b>   {{$skin->install_count}} fois
-        </p>
-
-        @if ( $skin->repo_url != null )
-        <p>
-            <b>  Contribuer : <a target="_blank" href="{{$skin->repo_url}}">{{str_limit($skin->repo_url,40)}}</a>  </b>
-        </p>
-        @endif
-
-        @if ( $skin->topic_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$skin->topic_url}}">Voir le topic jvc  <i class="fa fa-gamepad"></i></a>  </b>
-        </p>
-        @endif
-
-        @if ( $skin->website_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$skin->website_url}}">Voir le site web  <i class="fa fa-globe"></i></a>  </b>
-        </p>
-        @endif
-
-        @if ( $skin->don_url != null )
-        <p>
-            <b>   <a target="_blank" class="btn btn-default" href="{{$skin->don_url}}">Faire un don au développeur  <i class="fa fa-heart"></i></a>  </b>
-        </p>
-        @endif
-                    </div>
-</div>
     </div>
 
     <div class="col-md-6">
-      <div class="panel-body desc">
-        @if( $skin->description != '' )
-         {!! (($skin->description )) !!}
-        @endif
-      </div>
+        <div class="panel-body desc">
+            @if( $skin->description != '' )
+            {!! (($skin->description )) !!}
+            @endif
+        </div>
 
     </div>
 </div>
@@ -141,59 +142,65 @@
 @if ((Auth::check() && Auth::user()->isAdmin()))
 <div class="row">
     <div class="col-md-6">
-      <div class="panel-body">
-          <div class="admin">
-        @if($skin->status == 0)
-        <div class="alert alert-warning" role="alert">
-            Ce skin est en attente de validation
-        </div>
-        @elseif($skin->status == 1)
-        <div class="alert alert-success" role="alert">
-            Ce skin a été validé.
-        </div>
-        @elseif($skin->status == 2)
-        <div class="alert alert-danger" role="alert">
-            Ce skin a été refusé.
-        </div>
-        @endif
-        <p>
-            Edition :
-            <a href="{{route('skin.edit',$skin->slug)}}" class="btn btn-primary">Editer</a>
-            <a href="{{route('skin.delete',$skin->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
+        <div class="panel-body">
+            <div class="admin">
+                @if($skin->status == 0)
+                <div class="alert alert-warning" role="alert">
+                    Ce skin est en attente de validation
+                </div>
+                @elseif($skin->status == 1)
+                <div class="alert alert-success" role="alert">
+                    Ce skin a été validé.
+                </div>
+                @elseif($skin->status == 2)
+                <div class="alert alert-danger" role="alert">
+                    Ce skin a été refusé.
+                </div>
+                @endif
+                <p>
+                    Edition :
+                    <a href="{{route('skin.edit',$skin->slug)}}" class="btn btn-primary">Editer</a>
+                    <a href="{{route('skin.delete',$skin->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
 
-            Validation :
-            <!--_TODO : confirm dialog-->
-            <a href="{{route('skin.validate',$skin->slug)}}" class="btn btn-success" data-toggle="confirmation" >Valider</a>
-            <a href="{{route('skin.refuse',$skin->slug)}}" class="btn btn-warning" data-toggle="confirmation" >Refuser</a>
-        </p>
+                    Validation :
+                    <!--_TODO : confirm dialog-->
+                    <a href="{{route('skin.validate',$skin->slug)}}" class="btn btn-success" data-toggle="confirmation" >Valider</a>
+                    <a href="{{route('skin.refuse',$skin->slug)}}" class="btn btn-warning" data-toggle="confirmation" >Refuser</a>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 @elseif ((Auth::check() && Auth::user()->id == $skin->user_id))
 <div class="row">
     <div class="col-md-6">
-        @if($skin->status == 0)
-        <div class="alert alert-warning" role="alert">
-            Ce skin est en attente de validation
+        <div class="panel-body">
+            <div class="admin">
+
+                @if($skin->status == 0)
+                <div class="alert alert-warning" role="alert">
+                    Votre skin est en attente de validation
+                </div>
+                @elseif($skin->status == 1)
+                <div class="alert alert-success" role="alert">
+                    Votre skin a été validé.
+                </div>
+                @elseif($skin->status == 2)
+                <div class="alert alert-danger" role="alert">
+                    Votre skin a été refusé.
+                </div>
+                @endif
+                <p>
+                    Edition :
+                    <a href="{{route('skin.edit',$skin->slug)}}" class="btn btn-primary">Editer</a>
+                    <a href="{{route('skin.delete',$skin->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
+                </p>
+            </div>
         </div>
-        @elseif($skin->status == 1)
-        <div class="alert alert-success" role="alert">
-            Ce skin a été validé.
-        </div>
-        @elseif($skin->status == 2)
-        <div class="alert alert-danger" role="alert">
-            Ce skin a été refusé.
-        </div>
-        @endif
-        <p>
-            Edition :
-            <a href="{{route('skin.edit',$skin->slug)}}" class="btn btn-primary">Editer</a>
-            <a href="{{route('skin.delete',$skin->slug)}}" class="btn btn-danger" data-toggle="confirmation" >Supprimer</a>
-        </p>
+
     </div>
-    </div>
 </div>
-</div>
-</div>
+
 @endif
 
 
