89aed9e20ea25fbabbde4cf4702b4f532fef5dee
Vito <fb@vito.fr.nf>
2017-02-23 13:56:14 +0100

    [admin] lien vers gestion commentaires

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::post('/skin/{slug}/comment', 'JvscriptController@storeComment')->name('sk
 //delete comment
 Route::get('/script/{slug}/comment/{comment_id}/delete', 'JvscriptController@deleteComment')->name('script.comment.delete')->middleware('auth');
 Route::get('/skin/{slug}/comment/{comment_id}/delete', 'JvscriptController@deleteComment')->name('script.comment.delete')->middleware('auth');
+//delete comments admin
 Route::get('/admin/comment/{comment_id}/delete', 'JvscriptController@adminDeleteComment')->name('admin.comment.delete')->middleware('auth');
 
 
