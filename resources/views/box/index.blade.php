52ab733235daffc6bae77e35586428deae476cd1
Nass O <nowzy1@gmail.com>
2017-02-28 17:03:41 +0100

    ajout compressed css + boite a idées

--- resources/views/box/index.blade.php
+++ resources/views/box/index.blade.php
@@ -13,10 +13,10 @@
 
     <div class="col-md-8 col-md-offset-2">
 
-        @if (session('message')) 
+        @if (session('message'))
         <div class="alert alert-success">
             {{ session('message') }}
-        </div> 
+        </div>
         @endif
 
         <p class="text-center">
@@ -24,41 +24,38 @@
             </a>
         </p>
         <?php
-        
+
         $types_label = ['Script', 'Skin'];
         ?>
 
         @foreach ($ideas as $key => $idea)
-        <div class="row">
-
+        <div class="row panel-body">
             <div class="col-xs-1" style="padding-top:18px">
-                <p class="text-center">
-
-                    <a href="{{route('box.like',['id' => $idea->id])}}"> <i class="fa fa-arrow-up" aria-hidden="true"></i>  </a> <br>
-                    {{$idea->likes()->where('liked',1)->count() - $idea->likes()->where('liked',0)->count()}} <br>
-                    <a href="{{route('box.dislike',['id' => $idea->id,'dislike' => true])}}"> <i class="fa fa-arrow-down" aria-hidden="true"></i> </a>
+                <p>
+                    <a class="like" href="{{route('box.like',['id' => $idea->id])}}"> <i class="fa fa-arrow-up" aria-hidden="true"></i>  </a>
+                    <br><b class="note">{{$idea->likes()->where('liked',1)->count() - $idea->likes()->where('liked',0)->count()}}</b><br>
+                    <a class="dislike" href="{{route('box.dislike',['id' => $idea->id,'dislike' => true])}}"> <i class="fa fa-arrow-down" aria-hidden="true"></i> </a>
                 </p>
             </div>
 
             <div class="col-xs-10">
-                <div class=" panel panel-default">
-                    <div class="panel-heading text-left " style='text-align: left'>
+                <div class="panel idea">
+                    <div class="panel-heading idea" style='text-align: left'>
                         [{{$types_label[$idea->type]}}]
+
+                        <!-- TODO: supprimer ce lien quand les commentaires accordéon sont prêts-->
                         <a href='{{route('box.show',$idea->id)}}'>{{$idea->title}}</a>
 
                         <span class="date pull-right">
-                            Par {{$idea->user()->first()->name}} le 
-                            {{$idea->created_at->format('d/m/Y')}} 
+                            Par {{$idea->user()->first()->name}} le
+                            {{$idea->created_at->format('d/m/Y')}}
                         </span>
                     </div>
 
-                    <div class="panel-body">{{$idea->description}}
-                    </div> 
-
+                    <div class="panel-body idea">{{$idea->description}}
+                    </div>
                 </div>
-
-            </div> 
-
+            </div>
         </div>
 
         @endforeach
