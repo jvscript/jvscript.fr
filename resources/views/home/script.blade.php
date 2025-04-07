f4f13b16d1eb48d926b87463300fe46ed242f8da
Victorien Plancke <victorien@plkproduction.com>
2025-04-07 21:22:09 +0200

    fix responsive

--- resources/views/home/script.blade.php
+++ resources/views/home/script.blade.php
@@ -3,13 +3,12 @@
         <a href="{{route('script.show',['slug' => $script->slug ])}}" class="">
 
             <div class="hover-caption hidden-xs">
-                <p class="pull-left">
                 <h4>{{$script->name}}
 
                     @if($script->pinned)
-                        <span class="pull-right">
-                            <i class="fa fa-solid fa-thumb-tack" title="Épinglé"></i>
-                        </span>
+                    
+                        <i class="fa fa-solid fa-thumb-tack" title="Épinglé"></i>
+                    
                     @endif
 
                 </h4>
@@ -17,18 +16,18 @@
                     <?php $note = round($script->note * 2) / 2; ?>
                     @for ($i = 1; $i <= $note ; $i++)
                         <i class="fa fa-star" aria-hidden="true"></i>
-                    @endfor
+                        @endfor
 
-                    <?php $stop = $i; ?>
+                        <?php $stop = $i; ?>
 
-                    @for ($i ; $i <= 5 ; $i++)
-                        @if($i == $stop && $note > ( $i -1 ) )
+                        @for ($i ; $i <= 5 ; $i++)
+                            @if($i==$stop && $note> ( $i -1 ) )
                             <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                        @else
+                            @else
                             <i class="fa fa-star-o" aria-hidden="true"></i>
-                        @endif
+                            @endif
 
-                    @endfor
+                            @endfor
 
                 </p>
                 <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
@@ -56,19 +55,19 @@
             <?php $src = $script->photo_url == null ? "/assets/images/script.jpg" : $script->photoSmall() ?>
             <div class="image">
                 @if($lazy)
-                    <img data-src="{{$src}}" class="b-lazy" alt="{{$script->name}} logo"/>
+                <img data-src="{{$src}}" class="b-lazy" alt="{{$script->name}} logo" />
                 @else
-                    <img src="{{$src}}" class="" alt="{{$script->name}} logo"/>
-            @endif
-            <!--<img src="{{$src}}" class="" alt="{{$script->name}} logo" />-->
+                <img src="{{$src}}" class="" alt="{{$script->name}} logo" />
+                @endif
+                <!--<img src="{{$src}}" class="" alt="{{$script->name}} logo" />-->
             </div>
 
             <div class="caption">
                 <h4>{{$script->name}}
                     @if($script->pinned)
-                        <span class="pull-right">
-                            <i class="fa fa-solid fa-thumb-tack" title="Épinglé"></i>
-                        </span>
+                    <span class="pull-right">
+                        <i class="fa fa-solid fa-thumb-tack" title="Épinglé"></i>
+                    </span>
                     @endif
                 </h4>
 
@@ -76,18 +75,18 @@
                     <?php $note = round($script->note * 2) / 2; ?>
                     @for ($i = 1; $i <= $note ; $i++)
                         <i class="fa fa-star" aria-hidden="true"></i>
-                    @endfor
+                        @endfor
 
-                    <?php $stop = $i; ?>
+                        <?php $stop = $i; ?>
 
-                    @for ($i ; $i <= 5 ; $i++)
-                        @if($i == $stop && $note > ( $i -1 ) )
+                        @for ($i ; $i <= 5 ; $i++)
+                            @if($i==$stop && $note> ( $i -1 ) )
                             <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                        @else
+                            @else
                             <i class="fa fa-star-o" aria-hidden="true"></i>
-                        @endif
+                            @endif
 
-                    @endfor
+                            @endfor
 
                 </p>
                 <p class="text-right"><i class="fa fa-download" aria-hidden="true"></i> {{$script->install_count}} </p>
